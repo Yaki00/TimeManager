5c16dd7a0bab430d314ec69c7ea33b58022ddda7
Yaki00 <bouchaouradam@gmail.com>
2026-01-04 14:11:06 +0100

    fix: gitignore

--- back/modules/auth/routes.js
+++ back/modules/auth/routes.js
@@ -1,10 +1,7 @@
 import { Router } from "express";
 import { register, login, refresh, me } from "./controller.js";
 import { requireAuth } from "./middleware.js";
-import {
-  authRateLimiter,
-  refreshRateLimiter,
-} from "../../core/rateLimit.js";
+import { authRateLimiter, refreshRateLimiter } from "../../core/rateLimit.js";
 
 const router = Router();
 
